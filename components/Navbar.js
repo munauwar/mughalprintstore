737e7a059e16b53fc229c78c868d6ff1ebb4f836
Munauwar <munauwar.mughal@hva.nl>
2022-02-09 17:16:06 +0100

    change responsive mobile

--- components/Navbar.js
+++ components/Navbar.js
@@ -53,7 +53,7 @@ const Navbar = () => {
                     </button>
                 </div>
             </div>
-            <div className={`${isOpen ? "flex" : "hidden"} justify-center items-center bg-gray-300 h-[calc(100vh_-_theme(space.24))]`}>
+            <div className={`${isOpen ? "flex" : "hidden"} justify-center items-center bg-gray-300 dark:bg-neutral-800 transition duration-500 dark:border-t-2 dark:border-black h-[calc(100vh_-_theme(space.24))]`}>
                 <ul className="w-screen md:hidden">
                     {
                         MenuItems.map((navigate, id) => (
@@ -64,6 +64,16 @@ const Navbar = () => {
                             </li>
                         ))
                     }
+                    <span onClick={() => setTheme(colorTheme)} className="cursor-pointer mx-auto justify-center flex">
+                        {colorTheme === "dark" ?
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                            </svg> :
+
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
+                            </svg>}
+                    </span>
                 </ul>
             </div>
         </nav >
